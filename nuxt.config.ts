8ac905e0f0e17fe553fc87598319d782870077ab
hks004 <halks5142@gmail.com>
2026-08-30 23:39:25 -0400

    Ajout du baseURL et ssr: false pour GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase'],
+  
+  ssr: false,
+
+  app: {
+    baseURL: '/Doublage-VFQ-Base/',
+    buildAssetsDir: '_nuxt/',
+  },
+
   supabase: {
     redirect: false // Désactive la redirection automatique si l'utilisateur n'est pas connecté
   }
